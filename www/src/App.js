311f97091cf624b6fe35f6a406d7a5fe789ec60e
Ilya Druzhinin <ilya.druzh@gmail.com>
2018-12-03 02:41:52 +0300

    some changes

--- www/src/App.js
+++ www/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 //import { wasmdraw } from 'rustfractals';
@@ -8,90 +8,107 @@ import './App.css';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      fractalArray: []
+        this.state = {
+            fractalArray: [],
+            windowHeight: 0,
+            windowWidth: 0,
+        }
+
+        this.getPixelsArray = this.getPixelsArray.bind(this);
+        this.componentToHex = this.componentToHex.bind(this);
+        this.rgbToHex = this.rgbToHex.bind(this);
+        this.updateCanvas = this.updateCanvas.bind(this);
+        this.setWindowSize = this.setWindowSize.bind(this);
     }
 
-    this.getPixelsArray = this.getPixelsArray.bind(this);
-    this.componentToHex = this.componentToHex.bind(this);
-    this.rgbToHex = this.rgbToHex.bind(this);
-    this.updateCanvas = this.updateCanvas.bind(this);
-  }
+    getPixelsArray = (result) => {
+        let counter = 0;
+        let tmpArr = [];
+        let resultArr = [];
 
-  getPixelsArray = (result) => {
-    let counter = 0;
-    let tmpArr = [];
-    let resultArr = [];
+        for (let value of result) {
+            counter++;
+            tmpArr.push(value);
 
-    for (let value of result) {
-      counter++;
-      tmpArr.push(value);
-
-      if (counter % 5 === 0) {
+            if (counter % 5 === 0) {
+                resultArr.push(tmpArr);
+                tmpArr = []
+            }
+        }
         resultArr.push(tmpArr);
-        tmpArr = []
-      }
+
+        return resultArr;
+    };
+
+
+    componentToHex = (x) => {
+        var hex = x.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
     }
-    resultArr.push(tmpArr);
 
-    return resultArr;
-  };
+    rgbToHex = (r, g, b) => {
+        return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
+    }
 
+    componentDidMount() {
 
-  componentToHex = (x) => {
-    var hex = x.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-  }
+        import('../node_modules/rustfractals')
+            .then(wasm => {
+                return wasm.wasmdraw(window.innerHeight, window.innerHeight, 100);
+            })
+            .catch(e => console.log("error: ", e))
+            .then(result => {
+                this.setState({
+                    fractalArray: this.getPixelsArray(result),
+                    windowHeight: window.innerHeight,
+                    windowWidth: window.innerHeight
+                });
+                this.updateCanvas();
+            });
+    }
 
-  rgbToHex = (r, g, b) => {
-    return "#" + this.componentToHex(r) + this.componentToHex(g) + this.componentToHex(b);
-  }
+    updateCanvas() {
+        // e.preventDefault();
+        const ctx = this.refs.canvas.getContext('2d');
 
-  componentDidMount() {
-    import('../node_modules/rustfractals')
-      .then(wasm => { return wasm.wasmdraw(1000, 1000, 100); })
-      .catch(e => console.log("error: ", e))
-      .then(result => {
-        this.setState({ fractalArray: this.getPixelsArray(result) });
-        this.updateCanvas();
-      });
-  }
+        this.state.fractalArray.forEach(function (el) {
+            // console.log("el: ", el)
+            let color = "#"
+                + (el[2].toString(16).length == 1 ? "0" + el[2].toString(16) : el[2].toString(16))
+                + (el[3].toString(16).length == 1 ? "0" + el[3].toString(16) : el[3].toString(16))
+                + (el[4].toString(16).length == 1 ? "0" + el[4].toString(16) : el[4].toString(16)); //this.rgbToHex(el[2], el[3], el[4]);
+            ctx.fillStyle = color;
+            ctx.fillRect(el[0], el[1], 1, 1);
+        })
+    }
 
-  updateCanvas() {
-    // e.preventDefault();
-    const ctx = this.refs.canvas.getContext('2d');
+    setWindowSize() {
+        this.setState({
+            windowHeight: document.innerHeight,
+            windowWidth: document.innerWidth
+        })
+    }
 
-    this.state.fractalArray.forEach(function (el) {
-      // console.log("el: ", el)
-      let color = "#"
-        + (el[2].toString(16).length == 1 ? "0" + el[2].toString(16) : el[2].toString(16))
-        + (el[3].toString(16).length == 1 ? "0" + el[3].toString(16) : el[3].toString(16))
-        + (el[4].toString(16).length == 1 ? "0" + el[4].toString(16) : el[4].toString(16)); //this.rgbToHex(el[2], el[3], el[4]);
-      ctx.fillStyle = color;
-      ctx.fillRect(el[0], el[1], 1, 1);
-    })
-  }
+    render() {
 
-  render() {
+        // var canvas = document.querySelector('.fractal');
+        // var ctx = canvas.getContext('2d');
 
-    // var canvas = document.querySelector('.fractal');
-    // var ctx = canvas.getContext('2d');
+        // this.state.fractalArray.forEach(function (el) {
+        //   let color = this.rgbToHex(el[2], el[3], el[4]);
+        //   ctx.fillStyle = color;
+        //   ctx.fillRect(el[0], el[1], 1, 1);
+        // })
 
-    // this.state.fractalArray.forEach(function (el) {
-    //   let color = this.rgbToHex(el[2], el[3], el[4]);
-    //   ctx.fillStyle = color;
-    //   ctx.fillRect(el[0], el[1], 1, 1);
-    // })
-
-    return (
-      <div className="App container-fluid">
-        <canvas className="fractal" ref="canvas" id="fractal" width="1000" height="1000"></canvas>
-      </div>
-    );
-  }
+        return (
+            <div className="App container-fluid">
+                <canvas className="fractal" ref="canvas" id="fractal" width={this.state.windowWidth} height={this.state.windowHeight}></canvas>
+            </div>
+        );
+    }
 }
 
 export default App;
